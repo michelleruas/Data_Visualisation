8bab697797e731e0230fdd053e9324bc1d17b1f2
michelleruas <michelleruas@Michelles-MacBook-Air.local>
2020-04-21 23:10:26 +0100

    Log_10 coversion

--- Murder_rate_plot.R
+++ Murder_rate_plot.R
@@ -6,7 +6,18 @@ x <- murders$population /10^6
 y <- murders$total
 plot(x, y)
 
-# a histogram of murder rates
-hist(murders$rate)
 
 
+population_in_millions <- murders$population/10^6
+total_gun_murders <- murders$total
+
+plot(population_in_millions, total_gun_murders)
+
+# Transform population using the log10 transformation and save to object log10_population
+log10_population <- log10(murders$population)
+
+# Transform total gun murders using log10 transformation and save to object log10_total_gun_murders
+log10_total_gun_murders <- log10(total_gun_murders)
+
+# Create a scatterplot with the log scale transformed population and murders 
+plot(log10_population, log10_total_gun_murders)
