94ed568b6cc69cc3e5241b310b2cbc214e2e909e
Michelle Ruas <michelle.ruas@gmail.com>
2020-04-25 23:31:26 +0100

    Gun murder visualisation

---
+++ Gun murder data visualisation using log scale .R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(dslabs)
+data(murders)
+
+murders %>% ggplot() +
+  geom_point(aes(x = population/10^6, y = total))
+
+# add points layer to predefined ggplot object
+p <- ggplot(data = murders)
+p + geom_point(aes(population/10^6, total))
+
+# add text layer to scatterplot
+p + geom_point(aes(population/10^6, total)) +
+  geom_text(aes(population/10^6, total, label = abb))
+
+# change the size of the points
+p + geom_point(aes(population/10^6, total), size = 3) +
+  geom_text(aes(population/10^6, total, label = abb))
+
+# move text labels slightly to the right
+p + geom_point(aes(population/10^6, total), size = 3) +
+  geom_text(aes(population/10^6, total, label = abb), nudge_x = 1)
+
+# simplify code by adding global aesthetic
+p <- murders %>% ggplot(aes(population/10^6, total, label = abb))
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 1.5)
+
+# log base 10 scale the x-axis and y-axis
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.05) +
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10")
+
+# efficient log scaling of the axes
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10()
+
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10() +
+  xlab("Population in millions (log scale)") +
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")
+
+
+
+# redefine p to be everything except the points layer
+p <- murders %>%
+  ggplot(aes(population/10^6, total, label = abb)) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10() +
+  xlab("Population in millions (log scale)") +
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")
+
+# make all points blue
+p + geom_point(size = 3, color = "blue")
+
+# color points by region
+p + geom_point(aes(col = region), size = 3)
+
+
+# define average murder rate
+r <- murders %>%
+  summarize(rate = sum(total) / sum(population) * 10^6) %>%
+  pull(rate)
+
+# basic line with average murder rate for the country
+p + geom_point(aes(col = region), size = 3) +
+  geom_abline(intercept = log10(r))    # slope is default of 1
+
+# change line to dashed and dark grey, line under points
+p + 
+  geom_abline(intercept = log10(r), lty = 2, color = "darkgrey") +
+  geom_point(aes(col = region), size = 3)
