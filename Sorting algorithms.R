a31aaf9b01f6d129b9797c68071867e1b45d7c34
Michelle Ruas <michelle.ruas@gmail.com>
2020-04-30 20:46:54 +0100

    Vector arithmetic operations

--- Sorting algorithms.R
+++ Sorting algorithms.R
@@ -43,4 +43,97 @@ pop <- sort(pop)
 pop[1]
 
 
+# Access population from the dataset and store it in pop
+pop <- murders$population
+
+# Use the command order to find the vector of indexes that order pop and store in object ord
+ord <- order(pop)
+
+# Find the index number of the entry with the smallest population size
+ord[1]
+
+# Find the index of the smallest value for variable total 
+which.min(murders$total)
+
+# Find the index of the smallest value for population
+which.min(murders$population)
+
+# Define the variable i to be the index of the smallest state
+i <- which.min(murders$population)
+
+# Define variable states to hold the states
+states <- murders$state
+
+# Use the index you just defined to find the state with the smallest population
+states[i]
+
+
+# Store temperatures in an object 
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Store city names in an object 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Create data frame with city names and temperature 
+city_temps <- data.frame(name = city, temperature = temp)
+
+# Define a variable states to be the state names
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <- rank(murders$population)
+
+# Create a data frame my_df with the state name and its rank
+my_df <- data.frame(name = states, rank = ranks)
+
+
+# Define a variable states to be the state names from the murders data frame
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <- rank(murders$population)
+
+# Define a variable ind to store the indexes needed to order the population values
+ind <- order(murders$population)
+
+# Create a data frame my_df with the state name and its rank and ordered from least populous to most 
+my_df <- data.frame(states = states, ranks = ind)
+
+# Create a data frame my_df with the state name and its rank and ordered from least populous to most 
+my_df <- data.frame(states = states[ind], ranks = ranks[ind])
+
+# Using new dataset 
+library(dslabs)
+data(na_example)
+
+# Checking the structure 
+str(na_example)
+
+# Find out the mean of the entire dataset 
+mean(na_example)
+
+# Use is.na to create a logical index ind that tells which entries are NA
+ind <- is.na(na_example)
+
+# Determine how many NA ind has using the sum function
+sum(ind)
+
+# Note what we can do with the ! operator
+x <- c(1, 2, 3)
+ind <- c(FALSE, TRUE, FALSE)
+x[!ind]
+
+# Create the ind vector
+library(dslabs)
+data(na_example)
+ind <- is.na(na_example)
+
+# We saw that this gives an NA
+mean(na_example)
+
+# Compute the average, for entries of na_example that are not NA
+mean(na_example[!ind])
+
+
+
 
---
+++ Vector Arithmetic.R
@@ -0,0 +1,23 @@
+library(dslabs)
+data(murders)
+
+# The name of the state with the maximum population is found by doing the following
+murders$state[which.max(murders$population)]
+
+# how to obtain the murder rate
+murder_rate <- murders$total / murders$population * 100000
+
+# ordering the states by murder rate, in decreasing order
+murders$state[order(murder_rate, decreasing=TRUE)]
+
+# Assign city names to `city` 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Store temperature values in `temp`
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Convert temperature into Celsius and overwrite the original values of 'temp' with these Celsius values
+temp <- (temp-32)*5/9
+
+# Create a data frame `city_temps` 
+city_temps <- data.frame(names = city, temperatures = temp)
