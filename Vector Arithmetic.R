13ae54c60af5738dee5bd22b5b83ea8a3619c846
Michelle Ruas <michelle.ruas@gmail.com>
2020-04-30 21:02:01 +0100

    Wrangling methods

--- Vector Arithmetic.R
+++ Vector Arithmetic.R
@@ -21,3 +21,37 @@ temp <- (temp-32)*5/9
 
 # Create a data frame `city_temps` 
 city_temps <- data.frame(names = city, temperatures = temp)
+
+# Define an object `x` with the numbers 1 through 100
+x <- c(1:100)
+
+# Compute the sum 
+sum(1/x^2)
+
+# Load the data
+library(dslabs)
+data(murders)
+murders
+# Store the per 100,000 murder rate for each state in murder_rate
+murder_rate <- murders$total/murders$population*100000
+
+# Calculate the average murder rate in the US 
+mean(murder_rate)
+
+x <- c(2, 43, 27, 96, 18)
+sort(x)
+order(x)
+rank(x)
+
+name <- c("Mandi", "Amy", "Nicole", "Olivia")
+distance <- c(0.8, 3.1, 2.8, 4.0)
+time <- c(10, 30, 40, 50)
+hours <- time/60
+speed <- distance/hours
+race <- data.frame(name = name, distance = distance, times = hours, speed = speed)
+race
+
+
+
+
+
---
+++ Wrangling Methods.R
@@ -0,0 +1,40 @@
+library(dslabs)
+data("murders")
+
+# defining murder rate as before
+murder_rate <- murders$total / murders$population * 100000
+# creating a logical vector that specifies if the murder rate in that state is less than or equal to 0.71
+index <- murder_rate <= 0.71
+# determining which states have murder rates less than or equal to 0.71
+murders$state[index]
+# calculating how many states have a murder rate less than or equal to 0.71
+sum(index)
+
+# creating the two logical vectors representing our conditions
+west <- murders$region == "West"
+safe <- murder_rate <= 1
+# defining an index and identifying states with both conditions true
+index <- safe & west
+murders$state[index]
+
+x <- c(FALSE, TRUE, FALSE, TRUE, TRUE, FALSE)
+which(x)    # returns indices that are TRUE
+
+# to determine the murder rate in Massachusetts we may do the following
+index <- which(murders$state == "Massachusetts")
+index
+murder_rate[index]
+
+# to obtain the indices and subsequent murder rates of New York, Florida, Texas, we do:
+index <- match(c("New York", "Florida", "Texas"), murders$state)
+index
+murders$state[index]
+murder_rate[index]
+
+x <- c("a", "b", "c", "d", "e")
+y <- c("a", "d", "f")
+y %in% x
+
+# to see if Boston, Dakota, and Washington are states
+c("Boston", "Dakota", "Washington") %in% murders$state
+
