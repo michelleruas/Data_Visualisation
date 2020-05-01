2925cec2de691c9cd2e33cf22ac79ebe0709418a
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-01 21:28:59 +0100

    Conditionals practise

--- Wrangling Methods.R
+++ Wrangling Methods.R
@@ -38,3 +38,21 @@ y %in% x
 # to see if Boston, Dakota, and Washington are states
 c("Boston", "Dakota", "Washington") %in% murders$state
 
+# Store the murder rate per 100,000 for each state, in `murder_rate`
+murder_rate <- murders$total / murders$population * 100000
+
+# Store the `murder_rate < 1` in `low` 
+low <- (murder_rate < 1)
+
+# Get the indices of entries that are below 1
+which(low)
+
+# Names of states with murder rates lower than 1
+murders$state[which(low)]
+
+# Create a vector ind for states in the Northeast and with murder rates lower than 1. 
+ind <- (murders$region == "Northeast" & low)
+
+# Names of states in `ind` 
+murders$state[ind]
+
