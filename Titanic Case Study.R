f3b7b717b448994edc55b8ba8f4d8f52c54956c3
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-04 18:03:28 +0100

    Completion of Titanic Case Study Data

--- Titanic Case Study.R
+++ Titanic Case Study.R
@@ -30,7 +30,26 @@ titanic %>%
   ggplot(aes(Fare, color= Survived)) +
   geom_boxplot()
 
+#Survival by fare type
+titanic %>%
+  ggplot(aes(Pclass, color= Survived)) +
+  geom_bar()
+
+#Survival by fare type
+titanic %>%
+  ggplot(aes(Pclass, color= Survived)) +
+  geom_bar(position = position_fill())
+
+#Survival by survived vs fare
+titanic %>%
+  ggplot(aes(Survived, color= Pclass)) +
+  geom_bar(position = position_fill())
 
 
+#Create a grid of density plots for age, filled by survival status, with count on the y-axis, faceted by sex and passenger class.
+titanic %>%
+  ggplot(aes(Age, color= Survived)) +
+  geom_density()+
+  facet_grid(Pclass ~ Sex)
 
 
